9e3c8c1fb46c3660bbaafd73c5bbfeaa72dcb678
crc9010@g.rit.edu <crc9010@g.rit.edu@5e29a917-6081-4674-cc27-7c07c0986549>
2012-05-02 04:30:00 +0000

    Cleaned up code, added a method for initializing a matrix, though it isn't perfect yet.

--- simplex_cpu.cpp
+++ simplex_cpu.cpp
@@ -7,19 +7,20 @@
 //===========================================================================//
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 #define INDEX(x,y) ((x) + width * (y))
+
+// #define DEBUG
 
 #ifdef DEBUG
   #define DBGPRINT(s) (cout << s << endl)
 #else
   #define DBGPRINT(S)
 #endif
-
-// Made global so that functions can get this value without it being passed in
-int num_rest = 3;
-int num_vars = 3;
 
 //===========================================================================//
 // print_matrix
@@ -31,10 +32,10 @@ int num_vars = 3;
 // @param  height     - the height of arr
 //===========================================================================//
 void print_matrix (float * arr, int width, int height) {
-  cout << "----------------------------------------\n";
+  cout << "------------------------------------------------------------------------------\n";
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      cout.width(5);
+      cout.width(10);
       // Print out 0 if element is close to 0
       if ( arr[INDEX(x,y)] < 1e-6 && arr[INDEX(x,y)] > -1e-6) {
         cout << 0;
@@ -47,13 +48,15 @@ void print_matrix (float * arr, int width, int height) {
     }
     cout << endl;
   }
-  cout << "----------------------------------------\n\n";
+  cout << "------------------------------------------------------------------------------\n\n";
+
 }
 
 //===========================================================================//
 // get_pivot_column_index
 //
-// Find the most negative number on the bottom row
+// Find the most negative number on the bottom row. In case of a tie, select
+// the column farthest to the right
 //
 // @param  arr        - the 1D array representation of the matrix
 // @param  width      - the width of arr
@@ -92,37 +95,37 @@ int get_pivot_column_index (float *arr, int width, int height) {
 //===========================================================================//
 // get_pivot_row_index
 //
-// Gets the pivot row based on the ratio
+// Gets the pivot row based on the ratio. In case of a tie, select the row 
+// furthest toward the bottom.
 //
-// @param  arr        - the 1D array representation of the matrix
-// @param  width      - the width of arr
-// @param  height     - the height of arr
+// @param  arr              - the 1D array representation of the matrix
+// @param  width            - the width of arr
+// @param  height           - the height of arr
+// @param  pivotColunmIndex - the column where the pivot row resides
 //
-// @return pivotRowIndex - the pivot row
+// @return pivotRowIndex    - the pivot row
 //===========================================================================//
-int get_pivot_row_index(float *arr, int width, int height){
+int get_pivot_row_index(float *arr, int width, int height, int pivotColumnIndex) {
+
   int solutionColumnIndex = width - 1;
 
-  // Pivot column index will be of the column where the least number in the 
-  // indicator row resides
-  int pivotColumnIndex = get_pivot_column_index(arr,width,height);
-
-  // pivot row index will be of the row whose ratio of solution col element 
+  // Pivot row index will be of the row whose ratio of solution col element 
   // to current element is the lowest
-  int pivotRowIndex=0;  //default
+  int pivotRowIndex = 0;
 
-  float lowestRatio=arr[0*width+solutionColumnIndex]/arr[0*width+pivotColumnIndex];//default
+  float lowestRatio = arr[INDEX(solutionColumnIndex, 0)] / arr[INDEX(pivotColumnIndex, 0)];
 
   // h=1: because computed the first ratio already in the two lines above
   // h<height-1: do not consider the last row because it is the indicator row
-  for(int h=1;h<height-1;h++){
-    float ratio = arr[h*width+solutionColumnIndex]/arr[h*width+pivotColumnIndex];
-    // update the pivot row
-    if(ratio<=lowestRatio){
-      lowestRatio=ratio;
-      pivotRowIndex=h;
+  for (int h = 1; h < height - 1; h++){
+    float ratio = arr[INDEX(solutionColumnIndex, h)] / arr[INDEX(pivotColumnIndex, h)];
+    // Update the pivot row
+    if (ratio <= lowestRatio){
+      lowestRatio   = ratio;
+      pivotRowIndex = h;
     }
   }
+
   return pivotRowIndex;
 }
 
@@ -139,23 +142,23 @@ void gaussian_eliminate (float *arr, int width, int height) {
   int counter = 0;
   float scale;
 
-  int pivotColumn = get_pivot_column_index(arr, width,height);
-  int pivotRow    = get_pivot_row_index(arr, width,height);
+  int pivotColumn = get_pivot_column_index (arr,width,height);
+  int pivotRow    = get_pivot_row_index (arr,width,height, pivotColumn);
 
   // Normalization
   scale = arr[pivotRow*width+pivotColumn];
   for (int col = 0; col < width; col++) {
-    arr[pivotRow*width + col] = arr[pivotRow*width + col] / scale;
+    arr[INDEX(col, pivotRow)] /= scale;
   }
 
-  float divider   = arr[pivotRow*width+pivotColumn];
+  float divider = arr[INDEX(pivotColumn, pivotRow)];
 
   // Row reduction
   for (int row = 0; row < height; row++) {
     if (row != pivotRow && divider != 0) {
       scale = arr[row*width+pivotColumn];
       for (int col = 0; col < width; col++) {
-       arr[row*width+col] = arr[row*width+col] - (arr[pivotRow*width+col] * (scale / divider));
+        arr[INDEX(col, row)] -= (arr[INDEX(col, pivotRow)] * (scale / divider));
       }
     }
   }
@@ -164,6 +167,9 @@ void gaussian_eliminate (float *arr, int width, int height) {
 //===========================================================================//
 // is_indicator_positive
 //
+// Loop through the bottom row of arr to see if all of the values are 
+// positive
+//
 // @param  arr        - the 1D array representation of the matrix
 // @param  width      - the width of arr
 // @param  height     - the height of arr
@@ -171,11 +177,11 @@ void gaussian_eliminate (float *arr, int width, int height) {
 // @return            - true if positive, false otherwise
 //===========================================================================//
 bool is_indicator_positive(float *arr, int width, int height){
-  int indicatorRowIndex=height-1;
-  bool is_ind_pos=true;
-  for(int col = 0; col<width;col++){
-    if(arr[indicatorRowIndex*width+col]<0){
-      is_ind_pos=false;
+  int indicatorRowIndex = height - 1;
+  bool is_ind_pos = true;
+  for (int col = 0; col < width; col++){
+    if (arr[INDEX(col, indicatorRowIndex)] < 0){
+      is_ind_pos = false;
     }
   }
   return is_ind_pos;
@@ -189,13 +195,64 @@ bool is_indicator_positive(float *arr, int width, int height){
 // @param  arr        - the 1D array representation of the matrix
 // @param  width      - the width of arr
 // @param  height     - the height of arr
+//
+// @return num_iterations - the number of iterations it took to complete
 //===========================================================================//
-void simplex_cpu (float *arr, int width, int height) {
+int simplex_cpu (float *arr, int width, int height) {
+
+  int num_iterations = 0;
 
   // Repeat until the bottom row is all positive
   while (!is_indicator_positive (arr, width, height)) {
+    DBGPRINT("Iteration " << num_iterations);
+    // Do the gaussian elimination part
     gaussian_eliminate (arr, width, height);
-    //print_matrix (arr, width, height);
+    // Increment the number of iterations
+    num_iterations++;
+    #ifdef DEBUG
+      print_matrix (arr, width, height);
+    #endif
+  }
+
+  return num_iterations;
+
+}
+
+//===========================================================================//
+// initialize_matrix
+//
+// Initialize the values of the matrix to random integers.
+//
+// NOTE: This isn't working properly yet (CODY)
+//
+// @param  arr        - the 1D array representation of the matrix
+// @param  width      - the width of arr
+// @param  height     - the height of arr
+//===========================================================================//
+void initialize_matrix (float *arr, int width, int height) {
+
+  // Seed random number generator
+  srand ( time(NULL) );
+
+  for (int x = 0; x < (width - 1) / 2; x++) {
+    for (int y = 0; y < height; y++) {
+      arr[INDEX(x,y)] = (rand() % 20) - 10;
+    }
+  }
+
+  for (int x = (width - 1) / 2; x < width - 1; x++) {
+    for (int y = 0; y < height; y++) {
+      DBGPRINT("x: " << x << ", y: " << y);
+      if (x - ((width - 1) / 2) == y) {
+        arr[INDEX(x,y)] = 1;
+      } else {
+        arr[INDEX(x,y)] = 0;
+      }
+    }
+  }
+
+  for (int y = 0; y < height - 1; y++) {
+    arr[INDEX(width-1, y)] = (rand() % 20) - 10;
   }
 
 }
@@ -210,49 +267,62 @@ int main() {
   cout << "Starting Simplex CPU Calculations\n";
 
   // Number of restrictions
-  num_rest = 2;
+  int num_rest = 3;
 
   // Number of variables
-  num_vars = 2;
+  int num_vars = 3;
 
-  int width  = (2*num_vars)+1;
+  int width  = (2*num_vars) + 1;
   int height = num_rest + 1;
   int size   = width * height;
 
-  /*
+  // This should be uncommented when initialize_matrix() is working
+  //float arr[size];
+
+  // The array is stored in a 1D vector, where the elements can be accessed by
+  // the INDEX(x,y) macro
+ 
+  // Maximize
   float arr[] = {  2,  1, 1, 1, 0, 0, 14,
                    4,  2, 3, 0, 1, 0, 28, 
                    2,  5, 5, 0, 0, 1, 30,
                   -1, -2, 1, 0, 0, 0, 0   };
-  */
 
-  /* // Maximize
+  /* 
+  // Maximize
   float arr[] = { 1,  1, 1, 0, 4,
                   2,  1, 0, 1, 5,
                  -3, -4, 0, 0, 0 };
   */
 
-  /*// Minimize
+  /*
+  // Minimize
   float arr[] = { 1,  2, 1, 0, 6,
                   3,  2, 0, 1, 12,
                  -2,  1, 0, 0, 0 };
   */
 
+  /*
   // Minimize
   float arr[] = { 1,  3, 1, 0, 2, 
                   2,  2, 0, 1, 5, 
                  -4, -3, 0, 0, 0 };
-
+  */
 
   cout << "Width:  " << width << endl;
   cout << "Height: " << height << endl << endl;
- 
+
+  // Not quite working as well as I'd like yet (Cody)
+  // initialize_matrix (arr, width, height);
+
   cout << "Starting matrix:\n";
   print_matrix (arr, width, height);
 
-  simplex_cpu (arr, width, height);
+  // Do the calculation
+  int num_iter = simplex_cpu (arr, width, height);
 
   cout << "\nSolution matrix:\n";
   print_matrix (arr, width, height);
 
+  cout << "The solution took " << num_iter << " iterations\n";
 }
