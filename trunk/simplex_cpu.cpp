a5d9d9935b0cd95c5ccfd5d6a9dffbb07b90b054
crc9010@g.rit.edu <crc9010@g.rit.edu@5e29a917-6081-4674-cc27-7c07c0986549>
2012-05-14 23:55:38 +0000

    Added common.h, containing the helper functions, and the CUDA file (not tested yet). The CPU version does work.

--- trunk/simplex_cpu.cpp
+++ trunk/simplex_cpu.cpp
@@ -7,132 +7,11 @@
 //===========================================================================//
 
 #include <iostream>
-#include <iomanip>
-#include <stdlib.h>
-#include <time.h>
+//#include <time.h>
+
+#include "common.h"
 
 using namespace std;
-
-#define INDEX(x,y) ((x) + width * (y))
-
-#define MAX_ITER 100000
-
-// #define USE_KNOWN_MATRIX
-
-// #define DEBUG
-
-#ifdef DEBUG
-  #define DBGPRINT(s) (cout << s << endl)
-#else
-  #define DBGPRINT(S)
-#endif
-
-//===========================================================================//
-// print_matrix
-//
-// Prints the array in a nice-to-read format
-//
-// @param  arr        - the 1D array representation of the matrix
-// @param  width      - the width of arr
-// @param  height     - the height of arr
-//===========================================================================//
-void print_matrix (float * arr, int width, int height) {
-  cout << "------------------------------------------------------------------------------\n";
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      cout.width(13);
-      // Print out 0 if element is close to 0
-      if ( arr[INDEX(x,y)] < 1e-6 && arr[INDEX(x,y)] > -1e-6) {
-        cout << 0;
-      } else {
-        cout << setprecision (5) << arr[INDEX(x,y)];
-      }
-      if (x != width-1) {
-        cout << ", ";
-      }
-    }
-    cout << ";" << endl;
-  }
-  cout << "------------------------------------------------------------------------------\n\n";
-
-}
-
-//===========================================================================//
-// get_pivot_column_index
-//
-// Find the most negative number on the bottom row. In case of a tie, select
-// the column farthest to the right
-//
-// @param  arr        - the 1D array representation of the matrix
-// @param  width      - the width of arr
-// @param  height     - the height of arr
-//
-// @return pivot_col  - the pivot column
-//===========================================================================//
-int get_pivot_column_index (float *arr, int width, int height) {
-  int pivot_col = 0;
-  float min_val = 0;
-
-  // Loop through each column
-  for (int x = 0; x < width; x++) {
-    // Save this for later use
-    float cur_val = arr[INDEX(x, height - 1)];
-
-    DBGPRINT("\nx: " << x);
-    DBGPRINT("cur_val: " << cur_val);
-    DBGPRINT("min_val: " << min_val);
-    DBGPRINT("pivot_col: " << pivot_col);
-    
-    // If current value is less than the min value, or x is 0, swap
-    // min_val with cur_val and set the pivot_col to the current x
-    // value
-    if ((cur_val <= min_val) || (x == 0)) {
-      DBGPRINT("swap!");
-      min_val = cur_val;
-      pivot_col = x;
-    }
-  }
-
-  // Return the pivot column
-  return pivot_col;
-}
-
-//===========================================================================//
-// get_pivot_row_index
-//
-// Gets the pivot row based on the ratio. In case of a tie, select the row 
-// furthest toward the bottom.
-//
-// @param  arr              - the 1D array representation of the matrix
-// @param  width            - the width of arr
-// @param  height           - the height of arr
-// @param  pivotColunmIndex - the column where the pivot row resides
-//
-// @return pivotRowIndex    - the pivot row
-//===========================================================================//
-int get_pivot_row_index(float *arr, int width, int height, int pivotColumnIndex) {
-
-  int solutionColumnIndex = width - 1;
-
-  // Pivot row index will be of the row whose ratio of solution col element 
-  // to current element is the lowest
-  int pivotRowIndex = 0;
-
-  float lowestRatio = arr[INDEX(solutionColumnIndex, 0)] / arr[INDEX(pivotColumnIndex, 0)];
-
-  // h=1: because computed the first ratio already in the two lines above
-  // h<height-1: do not consider the last row because it is the indicator row
-  for (int h = 1; h < height - 1; h++){
-    float ratio = arr[INDEX(solutionColumnIndex, h)] / arr[INDEX(pivotColumnIndex, h)];
-    // Update the pivot row
-    if (ratio <= lowestRatio){
-      lowestRatio   = ratio;
-      pivotRowIndex = h;
-    }
-  }
-
-  return pivotRowIndex;
-}
 
 //===========================================================================//
 // gaussian_eliminate
@@ -144,7 +23,6 @@ int get_pivot_row_index(float *arr, int width, int height, int pivotColumnIndex)
 // @param  height     - the height of arr
 //===========================================================================//
 void gaussian_eliminate (float *arr, int width, int height) {
-  int counter = 0;
   float scale;
 
   int pivotColumn = get_pivot_column_index (arr,width,height);
@@ -170,29 +48,6 @@ void gaussian_eliminate (float *arr, int width, int height) {
 }
 
 //===========================================================================//
-// is_indicator_positive
-//
-// Loop through the bottom row of arr to see if all of the values are 
-// positive
-//
-// @param  arr        - the 1D array representation of the matrix
-// @param  width      - the width of arr
-// @param  height     - the height of arr
-//
-// @return            - true if positive, false otherwise
-//===========================================================================//
-bool is_indicator_positive(float *arr, int width, int height){
-  int indicatorRowIndex = height - 1;
-  bool is_ind_pos = true;
-  for (int col = 0; col < width; col++){
-    if (arr[INDEX(col, indicatorRowIndex)] < 0){
-      is_ind_pos = false;
-    }
-  }
-  return is_ind_pos;
-}
-
-//===========================================================================//
 // simplex_cpu
 //
 // Puts the whole Simplex Algorithm together
@@ -210,13 +65,11 @@ int simplex_cpu (float *arr, int width, int height) {
   // Repeat until the bottom row is all positive
   while (!is_indicator_positive (arr, width, height)) {
 
-    // *** PARALLELIZE ME! ***
     // If number of iterations exceed the threshold, no solutions were found
     if (num_iterations > MAX_ITER) {
       return num_iterations;
     }
 
-    // *** PARALLELIZE ME! ***
     DBGPRINT("Iteration " << num_iterations);
     // Do the gaussian elimination part
     gaussian_eliminate (arr, width, height);
@@ -229,54 +82,6 @@ int simplex_cpu (float *arr, int width, int height) {
   }
 
   return num_iterations;
-
-}
-
-//===========================================================================//
-// initialize_matrix
-//
-// Initialize the values of the matrix to random integers.
-//
-// NOTE: This isn't working properly yet (CODY)
-//
-// @param  arr        - the 1D array representation of the matrix
-// @param  width      - the width of arr
-// @param  height     - the height of arr
-//===========================================================================//
-void initialize_matrix (float *arr, int width, int height) {
-
-  // Seed random number generator
-  srand ( time(NULL) );
-
-  for (int x = 0; x < (width - 1) / 2; x++) {
-    for (int y = 0; y < height; y++) {
-      if (y == height-1) {
-        arr[INDEX(x,y)] = (rand() % 20) - 10;
-      } else {
-        arr[INDEX(x,y)] = (rand() % 20);
-      }
-    }
-  }
-
-  for (int x = (width - 1) / 2; x < width - 1; x++) {
-    for (int y = 0; y < height; y++) {
-      DBGPRINT("x: " << x << ", y: " << y);
-      if (x - ((width - 1) / 2) == y) {
-        arr[INDEX(x,y)] = 1;
-      } else {
-        arr[INDEX(x,y)] = 0;
-      }
-    }
-  }
-
-  for (int y = 0; y < height; y++) {
-    if (y == height-1) {
-      arr[INDEX(width-1, y)] = 0;
-    } else {
-      arr[INDEX(width-1, y)] = (rand() % 20);
-    }
-  }
-
 }
 
 //===========================================================================//
@@ -288,16 +93,13 @@ int main() {
 
   cout << "Starting Simplex CPU Calculations\n";
 
-#ifndef USE_KNOWN_MATRIX
-  // Number of restrictions
-  int num_rest = 5;
-
-  // Number of variables
-  int num_vars = 5;
-#else
-  int num_rest = 3;
-  int num_vars = 3;
-#endif
+  #ifndef USE_KNOWN_MATRIX
+    int num_rest = 5; // Number of restrictions
+    int num_vars = 5; // Number of variables
+  #else
+    int num_rest = 3;
+    int num_vars = 3;
+  #endif
 
   int width  = (2*num_vars) + 1;
   int height = num_rest + 1;
@@ -360,11 +162,11 @@ int main() {
   } else {
     cout << "\nSolution matrix:\n";
     print_matrix (arr, width, height);
-
     cout << "The solution took " << num_iter << " iterations\n";
   }
 
   #ifndef USE_KNOWN_MATRIX
     free (arr);
   #endif
+  
 }
